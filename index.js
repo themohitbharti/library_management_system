eddf760391fcfcd7023ceac785ca9cb76b0bdec6
Mohit Bharti <mohitbharti373@gmail.com>
2023-10-16 13:01:49 +0530

    added patch route to update a specific field using isbn_no

--- index.js
+++ index.js
@@ -46,34 +46,6 @@ const bookSchema = new mongoose.Schema({
 
 const books= mongoose.model("bookdetails",bookSchema);
 
-// let posts = [
-//   {
-//     id: 1,
-//     title: "The Rise of Decentralized Finance",
-//     content:
-//       "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
-//     author: "Alex Thompson",
-//     date: "2023-08-01T10:00:00Z",
-//   },
-//   {
-//     id: 2,
-//     title: "The Impact of Artificial Intelligence on Modern Businesses",
-//     content:
-//       "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
-//     author: "Mia Williams",
-//     date: "2023-08-05T14:30:00Z",
-//   },
-//   {
-//     id: 3,
-//     title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
-//     content:
-//       "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
-//     author: "Samuel Green",
-//     date: "2023-08-10T09:15:00Z",
-//   },
-// ];
-
-// let lastId = 3;
 
 
 app.use(bodyParser.json());
@@ -110,18 +82,6 @@ app.get("/books", async (req, res) => {
   });
 
 
-// app.get("/books",async(req,res)=>{
-//     const allbooks = await books.find({});
-//     res.json(allbooks);
-//     console.log(allbooks);
-// });
-
-
-// app.get("/posts/:id",(req,res)=>{
-//    const id= req.params.id;
-//    const postbyid=posts.find((post)=> post.id==id);
-//    res.json(postbyid);
-// });
 
 
 app.post("/books",async(req,res)=>{
@@ -143,28 +103,12 @@ app.post("/books",async(req,res)=>{
 });
 
 
-// app.patch("/posts/:id",(req,res)=>{
-
-//   const post = posts.find((p) => p.id === parseInt(req.params.id));
-//   if (!post) return res.status(404).json({ message: "Post not found" });
-//   if (req.body.title) post.title = req.body.title;
-//   if (req.body.content) post.content = req.body.content;
-//   if (req.body.author) post.author = req.body.author;
-
-//   res.json(post);
-// });
 
 
 
 
 
-// app.delete("/posts/:id",(req,res)=>{
-//   const id= req.params.id;
-//   const postind=posts.findIndex((post)=> post.id==id);
-//   if (postind === -1) return res.status(404).json({ message: "Post not found" });
-//   posts.splice(postind,1);
-//   res.json({message:"post deleted"});
-// });
+
 
 
 app.delete("/books/:isbn_no", async (req, res) => {
@@ -250,7 +194,36 @@ app.delete("/books/:isbn_no", async (req, res) => {
 //       return res.status(500).json({ error: 'Internal server error' });
 //     }
 //   });
+
+
+app.patch("/books/:isbn_no", async (req, res) => {
+    const isbn = req.params.isbn_no;
+    
+    const updatedFields = req.body.genre;
+    
+   
+    try {
+      const updatedBook = await books.findOneAndUpdate(
+        { isbn_no: isbn },
+        { $set: { genre: updatedFields} },
+        { new: true }
+      );
   
+      if (!updatedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+  
+      res.json({ message: "Book updated", updatedBook });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  });
+  
+
+
+  
+ 
 
 
 
